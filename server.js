4a0d5b5c1a3595f04d47a6fa78d85daecffd039e
unknown <ahedqaood19@gmail.com>
2019-08-15 23:07:04 +0300

    handle posting messages using POST request when *send* button is pressed

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-const express = require('express');
+const bodyParser = require('body-parser');
+const express    = require('express');
+const app        = express();
+
+app.use(express.static('static'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 var messages = [
-    {name: 'Ahed',   messages: 'Hello'},
-    {name: 'Faisal', messages: 'Hi'}
+    {name: 'Ahed',   message: 'Hello'},
+    {name: 'Faisal', message: 'Hi'}
 ]
-
-const app = express();
-app.use(express.static('static'));
 
 app.get('/messages', (req, res) => {
     res.send(messages);
+});
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body);
+    res.sendStatus(200);
 });
 
 app.listen(3010);
