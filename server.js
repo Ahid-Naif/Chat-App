e4d6c83402a57d7ca8682bc88aca377317db8da6
unknown <ahedqaood19@gmail.com>
2019-08-15 22:04:25 +0300

    handle getting messages using GET request

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
+var messages = [
+    {name: 'Ahed',   messages: 'Hello'},
+    {name: 'Faisal', messages: 'Hi'}
+]
+
 const app = express();
 app.use(express.static('static'));
 
 app.get('/messages', (req, res) => {
-    res.send("It's working!!!");
+    res.send(messages);
 });
 
 app.listen(3010);
